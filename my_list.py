10f4721c84075c9ae06fb6694554afbd226b8273
Rachael731 <rachaeljelagat47@gmail.com>
2024-10-04 17:20:45 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,27 @@
+#1 Create an empty list
+my_list= []
+
+#2 Append elements 10, 20, 30, 40 to my_list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#3 Insert the value 15 at the second position
+my_list.insert(1,15)
+
+#4 Extend the list with 50, 60, 70
+my_list.extend([50, 60, 70])
+
+#5 Remove the lest element
+my_list.pop()
+
+# Sort the list in ascending order
+my_list.sort()
+
+#6 Find and print the index of the value 30
+index_of_30= my_list.index(30)
+print (index_of_30)
+
+
+
